e858ffc130055ad5957dcc55fb3c83f877121464
Hadian Alkani <sapatekno@gmail.com>
2024-01-27 14:06:47 +0700

    Real Device Check - Update check for popular emulator

--- android/src/main/kotlin/com/example/device_safety_info/realdevice/RealDeviceCheck.kt
+++ android/src/main/kotlin/com/example/device_safety_info/realdevice/RealDeviceCheck.kt
@@ -4,19 +4,38 @@ import android.os.Build
 
 class RealDeviceCheck {
 
-    companion object{
+    companion object {
 
         fun isRealDevice(): Boolean {
-            return (Build.FINGERPRINT.startsWith("generic")
+            return ((Build.BRAND.startsWith("generic")
+                    && Build.DEVICE.startsWith("generic"))
+                    || Build.FINGERPRINT.startsWith("generic")
                     || Build.FINGERPRINT.startsWith("unknown")
-                    || Build.MODEL.contains("google_sdk")
+                    || Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+                    || Build.FINGERPRINT.startsWith(":user/release-keys")
+                    || Build.HARDWARE.contains("goldfish")
+                    || Build.HARDWARE.contains("ranchu")
+                    || Build.HARDWARE.contains("vbox86")
+                    || Build.MODEL.contains("google_sdk ")
                     || Build.MODEL.contains("Emulator")
                     || Build.MODEL.contains("Android SDK built for x86")
                     || Build.MANUFACTURER.contains("Genymotion")
-                    || Build.MODEL.startsWith("sdk_")
-                    || Build.DEVICE.startsWith("emulator"))
-                    || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
-                    || "google_sdk" == Build.PRODUCT
+                    || Build.MANUFACTURER.contains("bluestacks", ignoreCase = true)
+                    || Build.PRODUCT.contains("sdk")
+                    || Build.PRODUCT.contains("vbox86p")
+                    || Build.PRODUCT.contains("emulator")
+                    || Build.PRODUCT.contains("simulator"))
+                    || Build.PRODUCT.startsWith("google_sdk")
+                    || Build.PRODUCT.contains("sdk_x86")
+                    || Build.PRODUCT.contains("vbox86p")
+                    || Build.BOARD.lowercase().contains("nox")
+                    || Build.BOOTLOADER.contains("nox", ignoreCase = true)
+                    || Build.HARDWARE.contains("nox", ignoreCase = true)
+                    || Build.PRODUCT.contains("nox", ignoreCase = true)
+                    || Build.SERIAL.contains("nox", ignoreCase = true)
+                    || Build.HOST == "ubuntu" // To Avoid False Positives on Oppo Realme Devices
+                    || Build.BOARD.contains("windows")
+                    || Build.HARDWARE.contains("windows")
         }
 
     }
